5c2b2324cd0a97e895541f92a11af1db85dec672
CubanSoil <sethumazibuko@gmail.com>
2025-12-08 14:18:47 +0000

    🦋 Add fade-in effect to filters container as well

--- components/cards/SummaryCard.jsx
+++ components/cards/SummaryCard.jsx
@@ -2,7 +2,7 @@ import styles from './SummaryCard.module.css';
 
 export default function SummaryCard({ title, value, subtitle }) {
   return (
-    <div className={styles['summary-card']} style={{'--d': '0.2s'}}>
+    <div className={styles['summary-card']} style={{'--d': '0.25s'}}>
       <div className={styles['summary-card-title']}>{title}</div>
       <div className={styles['summary-card-value']}>
         {value !== undefined && value !== null ? value : "—"}
